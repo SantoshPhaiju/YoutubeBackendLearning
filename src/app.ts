45b644e2eae5c310d56790c85f81c04df52e7190
Santosh Phaiju <92672422+SantoshPhaiju@users.noreply.github.com>
2024-10-12 20:50:09 +0545

    made some config in app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,30 @@
-import express, { Express } from 'express';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Express } from 'express';
 import { appConfig } from './config/config';
 
 const app: Express = express();
 
 const { corsOrigin } = appConfig;
 
-app.use(cors({
-    origin: corsOrigin,
-    credentials: true,
-}));
-
-app.use(express.json());
+app.use(
+    cors({
+        origin: corsOrigin,
+        credentials: true,
+    })
+);
+app.use(
+    express.json({
+        limit: '1mb',
+    })
+);
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: '1mb',
+    })
+);
+app.use(express.static('public'));
+app.use(cookieParser());
 
 export default app;
