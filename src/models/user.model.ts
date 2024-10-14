335117728c37c16a224e88623029e1a9943f01bf
Santosh Phaiju <92672422+SantoshPhaiju@users.noreply.github.com>
2024-10-14 15:22:57 +0545

    added jwtsecret, written some methods and hooks in userschema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
@@ -47,5 +48,19 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+});
+
+userSchema.methods.comparePassword = async function (
+    password: string
+): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model('User', userSchema);
